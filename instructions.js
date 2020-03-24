4c91bcbdd86212c9febe9e7e20c30f55671393da
Jacob Wise <>
2020-03-24 18:50:09 -0400

    adding config stub

---
+++ instructions.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const path = require("path");
+
+module.exports = async cli => {
+  try {
+    await cli.copy(
+      path.join(__dirname, "stubs/firebase.js"),
+      path.join(cli.helpers.configPath(), "firebase.js")
+    );
+    cli.command.completed("create", "config/firebase.js");
+  } catch (error) {
+    console.log(error);
+  }
+};
