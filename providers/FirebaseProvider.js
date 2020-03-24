7461c0d9689c6bc21cf6ccf4525f691fea99d535
Jacob Wise <>
2020-03-24 18:23:44 -0400

    adding new admin provider

--- providers/FirebaseProvider.js
+++ providers/FirebaseProvider.js
@@ -1,12 +1,12 @@
 "use strict";
 const { ServiceProvider } = require("@adonisjs/fold");
-const Firebase = require("firebase");
+const firebase = require("firebase");
 
 class FirebaseProvider extends ServiceProvider {
   register() {
     this.app.singleton("Adonis/Addons/Firebase", () => {
       const Config = this.app.use("Adonis/Src/Config");
-      return new Firebase(Config);
+      return firebase.initializeApp(Config);
     });
 
     this.app.alias("Adonis/Addons/Firebase", "Firebase");
